ef591e1de09e9f733bb4eaa943e87f73f41a826c
Jose Ortega <jo.manuel.ortega@gmail.com>
2024-05-27 11:36:59 -0600

    Add contains duplicate

--- others.py
+++ others.py
@@ -176,13 +176,47 @@ assert isValid("()[]{}") == True
 
 
 '''
+leetcode: https://leetcode.com/problems/contains-duplicate-ii/description/
+hackerrank: ?
+
+Problem: Given an integer array nums and an integer k, return true 
+if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+'''
+
+def containsNearbyDuplicate(nums, k):
+    
+    hm = {}    
+    for i in range(len(nums)):            
+        if nums[i] in hm:
+            if abs(i - hm[nums[i]]) <= k:
+                return True
+
+        hm[nums[i]] = i
+
+    return False
+
+
+'''
 leetcode: ?
 hackerrank: ?
 
 Problem: 
 
-Example: 
+Example 1:
 Input: 
-Output: 
-'''
+Output:
 
+'''
