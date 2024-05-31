00a97ae161c6dd72e15d3653e10a4713d2d36a98
Jose Ortega <jo.manuel.ortega@gmail.com>
2024-05-30 20:58:41 -0600

    add is subsequence

--- others.py
+++ others.py
@@ -210,6 +210,36 @@ def containsNearbyDuplicate(nums, k):
 
 
 '''
+leetcode: https://leetcode.com/problems/is-subsequence/description
+hackerrank: ?
+
+Problem: A subsequence of a string is a new string that is formed from the original 
+string by deleting some (can be none) of the characters without disturbing the 
+relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+
+'''
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+          
+        arr = [x for x in s]
+        for char in t:
+            if len(arr) > 0 and char == arr[0]:
+                arr.pop(0)
+
+        return len(arr) == 0
+
+'''
 leetcode: ?
 hackerrank: ?
 
